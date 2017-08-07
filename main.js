c3fe3dead75f99fbae61f8f481c9ef798308f59d
Quentin Freire Novo <yushi76240@gmail.com>
2017-08-07 16:31:39 +0200

    Update

--- main.js
+++ main.js
@@ -25,28 +25,14 @@ client.on("message", (message) => {
       if (err) throw err;
       console.log('The file has been saved!');
     });
-    message.channel.send("Event Created Call: " + name + " The :"+ datess + " At: " + hours + "Hours");
-  } else
-  if (message.content.startsWith(config.prefix + "DeleteEvent")) { // !DeleteEvent NomDelevent
-    if(message.author.id !== config.ownerID) return;
-    const args = message.content.split(/\s+/g);
-    let name = args[1];
-    // delete the json file
-    fs.unlink('./Events/' + name + '.json', function(error) {
-    if (error) {
-        throw error;
-        console.log('The file has been deleted!');
-
-    }
-});
-    message.channel.send("Events : " + name + "As been deleted");
+    message.channel.send("Event Created Call: " + name + " The : "+ datess + " At: " + hours + " Hours");
   } else
   if(message.content.startsWith(config.prefix + "Accepted")) { // !Accepted NomDelevent Role
     const args = message.content.split(/\s+/g);
+    // do an if the role is not dps heal or tank
     let eventname = args[1];
     let playername = message.author.username;
     let role = args[2];
-    console.log(playername);
     const jsonFile = require('./Events/' + eventname + '.json');
     jsonFile.PlayersAccepted.push(playername);
     jsonFile.PlayersAcceptedRole.push(role);
@@ -56,11 +42,12 @@ client.on("message", (message) => {
       console.log('The file has been saved!');
     });
     // add in the array call "PlayersAccepted" on the correct json file the username and the role on the PlayersAcceptedRole (should have the same index)
-    message.channel.send("Player :" + playername + "Join the event: " + eventname);
+    message.channel.send("Player : " + playername + " Join the event: " + eventname + " As : " + role);
   }
   else
   if(message.content.startsWith(config.prefix + "Undecided")) { // !NotSure NomDelevent Role
     const args = message.content.split(/\s+/g);
+
     let eventname = args[1];
     let role = args[2];
     let playername = message.author.username;
@@ -72,14 +59,27 @@ client.on("message", (message) => {
       console.log('The file has been saved!');
     });
     // add in the array call "PlayersNotSure" on the correct json file the username and the role on the PlayersAcceptedRole (should have the same index)
-    message.channel.send("Player :" + message.author.username + "Join the event " + eventname + "as undecided");
+    message.channel.send("Player : " + message.author.username + " Join the event " + eventname + " as undecided" + " As : " + role);
   }
   else
   if(message.content.startsWith(config.prefix + "Read")) { // !Read NomDelevent
     const args = message.content.split(/\s+/g);
     let eventname = args[1];
     const jsonFile = require('./Events/' + eventname + '.json');
-    message.channel.send("" + JSON.stringify(jsonFile));
+    message.channel.send("Event name : " + JSON.stringify(jsonFile.Name));
+    message.channel.send("Date of the event: " + JSON.stringify(jsonFile.dates));
+    message.channel.send("Hours of the event: " + JSON.stringify(jsonFile.Hours));
+    message.channel.send("Player Accepted : ");
+    for (i = 0; i < jsonFile.PlayersAccepted.length; i++) {
+    message.channel.send("" + JSON.stringify(jsonFile.PlayersAccepted[i]) + " " +JSON.stringify(jsonFile.PlayersAcceptedRole[i]));
+    }
+    message.channel.send("Player Undecided : ");
+    for (i = 0; i < jsonFile.PlayersNotSure.length; i++) {
+    message.channel.send("" + JSON.stringify(jsonFile.PlayersNotSure[i]) + " " +JSON.stringify(jsonFile.PlayersNotSureRole[i]));
+    }
+
+
+  //  message.channel.send("" + JSON.stringify(jsonFile));
   }
 
 
