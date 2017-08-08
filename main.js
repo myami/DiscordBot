dd384bf9f78f7e7544ae613701d51081e0edc442
Quentin Freire Novo <yushi76240@gmail.com>
2017-08-08 14:06:10 +0200

    Update

--- main.js
+++ main.js
@@ -2,20 +2,22 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require("fs")
- // TODO: Make on the config.json the role by default : Dps,Tank,Heal . But people can change them on it .
- // TODO: Avoid one player can register multiple time to an event .
- // TODO: A way to remove someone on the list with a command like " !Remove NameofTheEvent NameOfThePlayer " (taking the index of the player and remove the role).
+ // TODO: Make on the config.json the role by default : Dps,Tank,Heal . But people can change them on it .(array loop to hit and compare the args) DONE!!!
+ // TODO: Avoid one player can register multiple time to an event .(loop through the array to compare the username)
+ // TODO: A way to remove someone on the list with a command like " !Remove NameofTheEvent NameOfThePlayer " (taking the index of the player and remove the role).(loop intp the array take the index and remove the name and the role)
  // TODO: A simple way to export for googlesheet .
  // TODO: Do a translate.json file to have commands and message in different language .
  // TODO: Simple way to transform the data to an html file for download and have a better view(Can replace googlesheet if didn't succed to made it work)
  // TODO: Simple integration to website  .
- // TODO: A way to add DKP or loot system to it (for MMORPG ).
+ // TODO: A way to add DKP or loot system to it (for MMORPG ).(add an array and use the index will neeed html file or googlesheet to looks better)
  // TODO: Add Mysql option ?
  // TODO: Clean Code.
 client.login(config.token);
 
 client.on("ready", () => {
   console.log("Bot Ready");
+  console.log("" + config.Role.length);
+
 });
 client.on("message", (message) => {
 
@@ -45,16 +47,21 @@ client.on("message", (message) => {
     let playername = message.author.username;
     let role = args[2];
     const jsonFile = require('./Events/' + eventname + '.json');
-    jsonFile.PlayersAccepted.push(playername);
-    jsonFile.PlayersAcceptedRole.push(role);
+    for (i = 0; i < config.Role.length; i++) {
+      if (role == config.Role[i]){
+        jsonFile.PlayersAccepted.push(playername);
+        jsonFile.PlayersAcceptedRole.push(role);
+        fs.writeFile('./Events/' + eventname + '.json', JSON.stringify(jsonFile), (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!');
+        });
+        // add in the array call "PlayersAccepted" on the correct json file the username and the role on the PlayersAcceptedRole (should have the same index)
+        message.channel.send("Player : " + playername + " Join the event: " + " ' " + eventname + " ' " + " As : " + role);
+        message.channel.send("Done !!!! ");
+      }
+    }
 
-    fs.writeFile('./Events/' + eventname + '.json', JSON.stringify(jsonFile), (err) => {
-      if (err) throw err;
-      console.log('The file has been saved!');
-    });
-    // add in the array call "PlayersAccepted" on the correct json file the username and the role on the PlayersAcceptedRole (should have the same index)
-    message.channel.send("Player : " + playername + " Join the event: " + " ' " + eventname + " ' " + " As : " + role);
-    message.channel.send("Done !!!! ");
+  message.channel.send("Invalid Role");
 
   }
   else
@@ -65,15 +72,22 @@ client.on("message", (message) => {
     let role = args[2];
     let playername = message.author.username;
     const jsonFile = require('./Events/' + eventname + '.json');
-    jsonFile.PlayersNotSure.push(playername);
-    jsonFile.PlayersNotSureRole.push(role);
-    fs.writeFile('./Events/' + eventname + '.json', JSON.stringify(jsonFile), (err) => {
-      if (err) throw err;
-      console.log('The file has been saved!');
-    });
-    // add in the array call "PlayersNotSure" on the correct json file the username and the role on the PlayersAcceptedRole (should have the same index)
-    message.channel.send("Player : " + message.author.username + " Join the event " + " ' " + eventname +" ' " + " as undecided" + " As : " + role);
-    message.channel.send("Done !!!! ");
+      for (i = 0; i < config.Role.length; i++) {
+          if (role == config.Role[i]){
+            jsonFile.PlayersNotSure.push(playername);
+            jsonFile.PlayersNotSureRole.push(role);
+            fs.writeFile('./Events/' + eventname + '.json', JSON.stringify(jsonFile), (err) => {
+              if (err) throw err;
+              console.log('The file has been saved!');
+            });
+            // add in the array call "PlayersAccepted" on the correct json file the username and the role on the PlayersAcceptedRole (should have the same index)
+            message.channel.send("Player : " + message.author.username + " Join the event " + " ' " + eventname +" ' " + " as undecided" + " As : " + role);
+            message.channel.send("Done !!!! ");
+          }
+        }
+
+      message.channel.send("Invalid Role");
+
 
   }
   else
@@ -103,7 +117,7 @@ client.on("message", (message) => {
     message.channel.send("4/ !Read NameofTheEvent");
     message.channel.send("5/ !Commands");
 
-    message.channel.send("You can choice 3 Role : Dps,Heal,Tank");
+    message.channel.send("You can choice 3 Role : DPS,HEAL,TANK");
     message.channel.send("For delete an event or change is content go into the bot folder call 'Events' and change or delete the .json file with the name of the event");
     message.channel.send("Everytime you do a commands you will get a message 'Done !!!!' to confirm it's made ");
     message.channel.send("Done !!!! ");
